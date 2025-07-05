86c4fc5dfcedb3c5d131b99a3812e66d085a65fb
PENEKhun <penekhun@gmail.com>
2025-07-05 12:57:30 +0900

    first commit

---
+++ src/main/java/kr/huni/springbatchsample/SpringbatchSampleApplication.java
@@ -0,0 +1,13 @@
+package kr.huni.springbatchsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbatchSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbatchSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/huni/springbatchsample/SpringbatchSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.huni.springbatchsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
